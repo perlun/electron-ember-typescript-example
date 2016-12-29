44a995ea680e4c8ea8f97cfead7e3886e01d1741
Per Lundberg <per.lundberg@ecraft.com>
2016-12-29 22:36:54 +0200

    Improved start page
    It's somewhat nicer to list the versions in a proper list, and also include the Ember version here.

--- app/pods/application/controller.ts
+++ app/pods/application/controller.ts
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     // Workaround since the TS definition for Window doesn't include process (which isn't available in regular browser contexts).
-    process: (<any>window).process
+    process: (<any>window).process,
+    emberVersion: Ember.VERSION
 });
